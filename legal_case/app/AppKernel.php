a561b1f0e1dace59930897b9e028ff567ff75263
Bianka Świderska <bianka.swiderska@gmail.com>
2017-11-12 02:03:40 +0100

    FOSBundle User registration and login in progress

--- legal_case/app/AppKernel.php
+++ legal_case/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MyBundle\MyBundle(),
             new EXS\BulkEmailCheckerBundle\EXSBulkEmailCheckerBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ legal_case/src/MyBundle/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MyBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mybundle_user';
+    }
+
+
+}
